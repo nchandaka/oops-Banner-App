9bd1a80890b4dec4c8c888fe7cc6203035eb6013
nchandaka <nchandakan@gmail.com>
2026-02-17 09:40:47 +0530

    UC2: ASCII Banner for OOPS implemented

---
+++ US 2.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * UC2 - Display OOPS Banner using ASCII art
+ * @author nchandaka
+ * @version 1.1
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *****    *****  ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println(" *****    *****    *       *****  ");
+
+    }
+}
+
+
